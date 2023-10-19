8d70e6947b34f35e2b72141ff3f6f09c5baa2f7a
Jinho Choi <jinho.choi@emory.edu>
2023-10-19 15:20:43 -0400

    Add word_types_with_tokenization.txt

--- src/text_processing.py
+++ src/text_processing.py
@@ -23,22 +23,73 @@ def top_k_words(word_counts: Counter[str, int], k: int, top=True):
     return sorted(word_counts.items(), key=lambda x: x[1], reverse=top)[:k]
 
 
+def delimit(token: str, delimiters: set[str]) -> list[str]:
+    i = next((i for i, c in enumerate(token) if c in delimiters), -1)
+    if i < 0: return [token]
+
+    tokens = []
+    if i > 0: tokens.append(token[:i])
+    tokens.append(token[i])
+    if i + 1 < len(token): tokens.extend(delimit(token[i + 1:], delimiters))
+    return tokens
+
+
+def postprocess(tokens: list[str]) -> list[str]:
+    i, new_tokens = 0, []
+    while i < len(tokens):
+        # apostrophe: 's
+        if i + 1 < len(tokens) and tokens[i] == "'" and tokens[i + 1].lower() == 's':
+            new_tokens.append(''.join(tokens[i:i + 2]))
+            i += 1
+        # numbers: [##], ###,###
+        elif i + 2 < len(tokens) and \
+                ((tokens[i] == '[' and tokens[i + 1].isnumeric() and tokens[i + 2] == ']') or \
+                 (tokens[i].isnumeric() and tokens[i + 1] == ',' and tokens[i + 2].isnumeric())):
+            new_tokens.append(''.join(tokens[i:i + 3]))
+            i += 2
+        # acronyms: U.S.
+        elif i + 3 < len(tokens) and ''.join(tokens[i:i + 4]) == 'U.S.':
+            new_tokens.append(''.join(tokens[i:i + 4]))
+            i += 3
+        else:
+            new_tokens.append(tokens[i])
+        i += 1
+    return new_tokens
+
+
+def tokenize(filename: str, delimiters: set[str]) -> list[str]:
+    tmp = open(filename).read().split()
+    return [token for t in tmp for token in postprocess(delimit(t, delimiters))]
+
 
 if __name__ == '__main__':
-    filename = 'dat/emory-wiki.txt'
+    filename = 'dat/text_processing/emory-wiki.txt'
+    delimiters = {'"', "'", '(', ')', '[', ']', ':', '-', ',', '.'}
 
-    # Word Counting
-    words = open(filename).read().split()
-    word_counts = Counter(words)
-    print('# of word tokens: {:3d}'.format(len(words)))
-    print('# of word types : {:3d}'.format(len(word_counts)))
-
-    topk = top_k_words(word_counts, 10)
-    for word, count in topk: print(word, count)
-    topk = top_k_words(word_counts, 10, False)
-    for word, count in topk: print(word, count)
-
+    # # Word Counting
+    # words = open(filename).read().split()
+    # word_counts = Counter(words)
+    # print('# of word tokens: {}'.format(len(words)))
+    # print('# of word types: {}'.format(len(word_counts)))
+    #
+    # topk = top_k_words(word_counts, 10)
+    # for word, count in topk: print(word, count)
+    #
+    # topk = top_k_words(word_counts, 10, False)
+    # for word, count in topk: print(word, count)
+    #
     # Tokenization
-    for key in sorted(word_counts.keys()): print(key)
+    # fout = open('dat/text_processing/word_types_wo_tokenization.txt', 'w')
+    # for key in sorted(word_counts.keys()): fout.write('{}\n'.format(key))
+    #
+    # tests = ['"R1:', '(R&D)', '15th-largest', 'Atlanta,', "Department's", 'activity"[26]', 'centers.[21][22]', '149,000', 'U.S.']
+    # for test in tests:
+    #     print('{} -> {}'.format(test, postprocess(delimit(test, delimiters))))
 
+    words = tokenize(filename, delimiters)
+    word_counts = Counter(words)
+    print('# of word tokens: {}'.format(len(words)))
+    print('# of word types: {}'.format(len(word_counts)))
 
+    fout = open('dat/text_processing/word_types_with_tokenization.txt', 'w')
+    for key in sorted(word_counts.keys()): fout.write('{}\n'.format(key))
