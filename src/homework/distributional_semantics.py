0a273e4414953c291b5345d54ebb090672bc3c01
Jinho Choi <jinho.choi@emory.edu>
2024-06-20 14:47:03 -0400

    Update HW1

--- src/homework/distributional_semantics.py
+++ src/homework/distributional_semantics.py
@@ -14,30 +14,65 @@
 # limitations under the License.
 # ========================================================================
 
-__author__ = 'Jinho D. Choi'
+__author__ = 'Paige Hendricks'
 
 import numpy as np
 
 
 def read_word_embeddings(filepath: str) -> dict[str, np.array]:
-    # To be filled
-    pass
+    embeddings = {}
+    for line in open(filepath, 'r'):
+        parts = line.strip().split()
+        word = parts[0]
+        vector = np.array([float(x) for x in parts[1:]], dtype=np.float32)
+        embeddings[word] = vector
+    return embeddings
 
-
+def cosine_similarity(u: np.array, v: np.array) -> float:
+    dot_product = np.dot(u, v)
+    norm_u = np.linalg.norm(u)
+    norm_v = np.linalg.norm(v)
+    if norm_u == 0 or norm_v == 0:
+        return 0.0
+    return dot_product / (norm_u * norm_v)
 def similar_words(word_embeddings: dict[str, np.array], target_word: str, threshold: float = 0.8) -> list[tuple[str, float]]:
-    # To be filled
-    pass
+    if target_word not in word_embeddings:
+        return []
+    target_embedding = word_embeddings[target_word]
+    similarities = []
+
+    for word, embedding in word_embeddings.items():
+        if word != target_word:
+            sim = cosine_similarity(target_embedding, embedding)
+            if sim >= threshold:
+                similarities.append((word, sim))
+
+    similarities.sort(key=lambda x: x[1], reverse=True)
+    return similarities
+
+
+def document_embedding(word_embeddings: dict[str, np.array], document: str) -> np.array:
+    embeddings = []
+    for word in document.split():
+        if word in word_embeddings:
+            embeddings.append(word_embeddings[word])
+    if embeddings:
+        return np.mean(embeddings, axis=0)
+    else:
+       return np.zeros_like(next(iter(word_embeddings.values())))
 
 
 def document_similarity(word_embeddings: dict[str, np.array], d1: str, d2: str) -> float:
-    # To be filled
-    pass
+    doc1_embedding = document_embedding(word_embeddings, d1)
+    doc2_embedding = document_embedding(word_embeddings, d2)
+
+    return cosine_similarity(doc1_embedding, doc2_embedding)
 
 
 if __name__ == '__main__':
     filepath = 'dat/word_embeddings.txt'
     word_embeddings = read_word_embeddings(filepath)
-    similar_words(word_embeddings, 'America')
+    print(similar_words(word_embeddings, 'America', 0.8))
     d1 = 'I love this movie'
     d2 = 'I hate this movie'
     document_similarity(word_embeddings, d1, d2)
--- src/homework/text_processing.py
+++ src/homework/text_processing.py
@@ -19,112 +19,22 @@ __author__ = 'Jinho D. Choi'
 import re
 
 
-def parse_numeral(numeral: str) -> int:
-    vals = {'I': 1, "V": 5, "X": 10}
-    prev = numeral[0]
-    val = vals[prev]
-
-    for i in numeral[1:]:
-        if vals[prev] < vals[i]: val -= vals[prev] * 2
-        prev = i
-        val += vals[i]
-
-    return val
-
-
 def chronicles_of_narnia(filepath: str) -> dict:
-    file = open(filepath, 'r')
-    info = {}
-    book, chapter, token_count = None, None, 0
-    chapters = []
-
-    re_title = re.compile(r"([\w ,:']+) \( (19\d\d) \)")  # (title, year)
-    re_chapter = re.compile(r'Chapter ([IVX]+)', re.IGNORECASE)  # (chapter numeral)
-
-    # helper internal functions
-    def close_chapter():
-        nonlocal token_count
-        nonlocal chapter
-        nonlocal chapters
-        chapter['token_count'] = token_count
-        chapters.append(chapter)
-        token_count = 0
-
-    def close_book():
-        nonlocal info
-        nonlocal book
-        nonlocal chapter
-        nonlocal chapters
-        close_chapter()  # add remaining chapter
-        chapters.sort(key=lambda x: x['number'])
-        info[book]['chapters'] = chapters
-        chapter = None
-        chapters = []
-
-    # read line by line
-    # if title, add chapters, parse title, parse year, and update current book
-    # else if chapter, add previous chapter w/ token count, parse roman numeral, parse title in next line, update token counter
-    # else add token count to current token counter
-    for line in file:
-        title_m = re_title.match(line)
-
-        if title_m:
-            if book:
-                close_book()
-
-            book = title_m.group(1)
-            info[book] = {
-                'title': book,
-                'year': int(title_m.group(2))
-            }
-        else:
-            chapter_m = re_chapter.match(line)
-
-            if chapter_m:
-                if chapter:
-                    close_chapter()
-
-                chapter = {
-                    'number': parse_numeral(chapter_m.group(1)),
-                    'title': file.readline().rstrip('\n')
-                }
-            else:
-                token_count += len(line.split(' '))
-
-    close_book()  # add final book
-
-    file.close()
-    return info
+    # To be updated
+    return dict()
 
 
-RE_Abbreviation = r'[A-Za-z\.]+\.'  # will also match end of sentence but is flexible and will match etc., etc
-RE_Apostrophe = r'(?<!\w)\'[\d\w]+'
-RE_Concatenation = r'(?i)\w+\'\w+|\b\w+[nmt]{2}[ae]\b|\b\w+[^aeiou][dt]a\b|cannot'  # hard coded cannot because funny/planning would be recognized w/ general
-RE_Hyperlink = r'[\w]+://(?:[\d\w]+\.)+\w+[\S]+'
-RE_Number = r'(?:\d+[,./\-]?)+'
-RE_Unit = r'[$#€£]\d+|(?<!\w)\d+[A-Za-z%]+'
+RE_Abbreviation = r'to be filled'
+RE_Apostrophe = r'to be filled'
+RE_Concatenation = r'to be filled'
+RE_Hyperlink = r'to be filled'
+RE_Number = r'to be filled'
+RE_Unit = r'to be filled'
 
 if __name__ == '__main__':
-    # txtfile = 'dat/chronicles_of_narnia_abridged.txt'
-    txtfile = 'dat/chronicles_of_narnia.txt'
-    d = chronicles_of_narnia(txtfile)
+    filepath = 'dat/chronicles_of_narnia.txt'
+    d = chronicles_of_narnia(filepath)
     print(d)
 
     r = re.compile(RE_Abbreviation)
-    print(r.match('U.S.A.'))
-    print(r.findall('I see Dr. Mike (M.D.) for my conditions while I\'m in the U.S.A.'))
-    r = re.compile(RE_Apostrophe)
-    print(r.match('\'80s'))
-    print(r.findall('I love to listen to \'80s music too, just \'cause! Who\'s your favorite artist?'))
-    r = re.compile(RE_Concatenation)
-    print(r.match('gonna'))
-    print(r.findall('Please don\'t touch that - y\'ardy know I\'m GONNA get mad.'))
-    r = re.compile(RE_Hyperlink)
-    print(r.match('https://emory.gitbook.io/nlp-essentials'))
-    print(r.findall('go to https://emory.gitbook.io/nlp-essentials to see the nlp course!'))
-    r = re.compile(RE_Number)
-    print(r.match('192.168.2.1'))
-    print(r.findall('Call 123-456-7890 or visit us at 20.43.154.3 for 1/2 off our $30 bagels!'))
-    r = re.compile(RE_Unit)
-    print(r.match('6000kg'))
-    print(r.findall('And the prize goes to #20'))
+    print(r.match('Dr.'))
