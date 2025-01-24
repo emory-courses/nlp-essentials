26f2b29a0da9b97c0cd2c2cc77a2609e53592f8c
Jinho D. Choi <jinho.choi@emory.edu>
2025-01-24 10:14:30 -0500

    Update homework 2

--- src/homework/language_models.py
+++ src/homework/language_models.py
@@ -23,21 +23,51 @@ INIT = '[INIT]'
 
 
 def bigram_model(filepath: str) -> Bigram:
-    # To be updated
-    pass
+    """
+    Build a bigram language model from text file using Laplace smoothing, normalization, and initial word probabilities.
+
+    :param filepath: Path to a text file.
+    :return: Nested dictionary where:
+             - Outer key is previous word (including INIT and UNKNOWN)
+             - Inner key is current word (including UNKNOWN)
+             - Value is P(current|previous) probability
+    """
+    # TODO: Task 1
+    return dict()
 
 
 def sequence_generator(bigram_probs: Bigram, init_word: str, length: int = 20) -> tuple[list[str], float]:
-    # To be updated
-    pass
+    """
+    Generate a sequence of specified length starting with given word.
+
+    :param bigram_probs: Bigram probabilities from bigram_model().
+    :param init_word: First word in sequence.
+    :param length: Number of words to generate.
+    :return: Tuple containing:
+             - list[str]: Generated sequence
+             - float: Log probability of sequence using natural log
+    """
+    # TODO: Task 2
+    return ([], 0.0)
 
 
-def sequence_generator_max(bigram_probs: Bigram, init_word: str, length: int = 20) -> tuple[list[str], float]:
-    # To be updated
-    pass
+def sequence_generator_plus(bigram_probs: Bigram, init_word: str, length: int = 20) -> tuple[list[str], float]:
+    """
+        Generate a sequence of specified length starting with given word, which generates sequences with
+        higher probability scores and better semantic coherence compared to sequence_generator().
+
+        :param bigram_probs: Bigram probabilities from bigram_model().
+        :param init_word: First word in sequence.
+        :param length: Number of words to generate.
+        :return: Tuple containing:
+                 - list[str]: Generated sequence
+                 - float: Log probability of sequence using natural log
+        """
+    # TODO: Extra Credit
+    return ([], 0.0)
 
 
 if __name__ == '__main__':
     filepath = 'dat/chronicles_of_narnia.txt'
     bigram_probs = bigram_model(filepath)
-    sequence_generator(bigram_probs, 'I')
+    sequence_generator(bigram_probs, 'You')
