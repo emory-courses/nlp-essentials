e831bff42f647f0462025cd585b6ef76adbaaadc
Jinho D. Choi <jinho.choi@emory.edu>
2025-11-24 18:22:13 -0500

    Update llm_api_access.py

--- src/llm_api_access.py
+++ src/llm_api_access.py
@@ -1,5 +1,5 @@
 # ========================================================================
-# Copyright 2025 REALITY Ministry
+# Copyright 2025 Emory University
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,3 +15,4 @@
 # ========================================================================
 
 __author__ = 'Jinho D. Choi'
+
